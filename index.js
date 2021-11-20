1afaebd0eb98647bf9612a1e6cba03f8591984c4
WilliamDenega <williamdenega@gmail.com>
2021-11-20 18:06:12 -0500

    commit tree 16a5088c43edb731c7e2eb28b5e396c36bf284a5

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 
+const record = [
+    {
+    year: "2018",
+    result: "L"
+    },
+    {
+    year: "2017",
+    result: "W"
+    },
+    {
+    year: "2016",
+    result: "N/A"
+    }
+    //...
+  ]
+function superbowlWin(record){
+    let result
+    record.find( record =>{
+        if(record.result === 'W'){
+        result = record.year}
+    })
+
+    return result
+ 
+}
